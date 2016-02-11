23fe09486c375f54cf656ad11fc7ad2c46486ad4
owhite01 <owhite.white@googlemail.com>
2016-02-11 17:43:44 +0000

    Added a text file to test the program

--- src/sml/Machine.java
+++ src/sml/Machine.java
@@ -1,7 +1,6 @@
 package sml;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 /*
@@ -37,7 +36,7 @@ public class Machine {
     public static void main(String[] args) {
 
         Machine m = new Machine();
-        Translator t = new Translator("test");
+        Translator t = new Translator("test01");
         t.readAndTranslate(m.getLabels(), m.getProg());
 
         System.out.println("Here is the program; it has " + m.getProg().size() + " instructions.");
@@ -46,8 +45,9 @@ public class Machine {
         System.out.println("Beginning program execution.");
         m.execute();
         System.out.println("Ending program execution.");
-
         System.out.println("Values of registers at program termination:");
+
+
         System.out.println(m.getRegisters() + ".");
     }
 
